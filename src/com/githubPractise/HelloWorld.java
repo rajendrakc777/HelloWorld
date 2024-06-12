de34dcd44bc88aa49496627f2b4e3f2972ec7e13
rajendrakc777 <35937249+rajendrakc777@users.noreply.github.com>
2024-06-12 17:50:02 -0400

    i add one line on main branch

--- src/com/githubPractise/HelloWorld.java
+++ src/com/githubPractise/HelloWorld.java
@@ -3,10 +3,10 @@ package com.githubPractise;
 public class HelloWorld {
 
 	
-	//this code is belong to master  branch
+	//this code is belong to main  branch
 	public static void main(String[] args) {
 		System.out.println("hellow world!!!!!!");
-		System.out.println("this code belong to master branch!!");
+		System.out.println("this code belong to main branch");
 		
 
 	}
